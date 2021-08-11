2a5048363e30572d22595934204ac5155654aa8a
evan <814422859@qq.com>
2021-08-11 17:15:11 +0800

    using 'Context' to do more refactor,2021-08-11 day two <https://geektutu.com/post/gee-day2.html\> done

--- ego/ego.go
+++ ego/ego.go
@@ -1,25 +1,35 @@
 package ego
 
 import (
-    "fmt"
+    //"fmt"
     "net/http"
+    //"context"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+// using Context instead, refactor 2021-07-29
+// type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
     // define a map which key-value pair is "string-HandleFunc" called "router"
-    router map[string]HandlerFunc
+    // using Context instead, refactor 2021-07-29
+    // router map[string]HandlerFunc
+    router *router
 }
 
 func New() *Engine {
-    return &Engine{router: make(map[string]HandlerFunc)}
+    // using Context instead, refactor 2021-07-29
+    //return &Engine{router: make(map[string]HandlerFunc)}
+    return &Engine{router: newRouter()}
 }
 
 // define a mathod named "addRoute" for "Engine" class(or you can call is struct)
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+    /* using Context instead, refactor 2021-07-29
     key := method + "-" + pattern
     engine.router[key] = handler
+    */
+    engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
@@ -38,10 +48,14 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    /*  using Context instead, refactor 2021-07-29
     key := req.Method + "-" + req.URL.Path
     if handler, ok := engine.router[key]; ok {
         handler(w, req)
     } else {
         fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
     }
+    */
+    c := newContext(w, req)
+    engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
+    //"fmt"
     "net/http"
-
+    //"context"
     "ego"
 )
 
@@ -22,19 +22,42 @@ func main() {
         }
     })
     */
+
+    /* using Context instead, refactor, 2021-07-29
     r.GET("/", indexHandler)
     r.GET("/hello", helloHandler)
+    */
     //end of refactor
+    r.GET("/", func(c *ego.Context) {
+        c.HTML(http.StatusOK, "<h1>HELLO EGO</h1>")
+        })
+    r.GET("/hello", func(c *ego.Context) {
+        // expect /hello?name=ego
+        c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"),c.Path)
+    })
+
+    r.POST("/login", func(c *ego.Context) {
+        c.JSON(http.StatusOK, ego.NewJSON {
+            "username": c.PostForm("username"),
+            "password": c.PostForm("password"),
+        })
+    })
+ 
     r.Run(":9999")
 }
 
 // handler echoes r.URL.Path
+/* using Context instead, refactor, 2021-07-29
 func indexHandler(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 }
+
+
 // handler echoes r.URL.Header
 func helloHandler(w http.ResponseWriter, req *http.Request) {
     for k, v := range req.Header {
         fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
     }
 }
+*/
+
